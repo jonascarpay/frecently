6f5c51119d0668b004ea4a5ae658b6181b6e5573
Jonas Carpay <jonascarpay@gmail.com>
2022-03-23 11:14:55 +0900

    Hourly/daily/monthly energy

--- app/Main.hs
+++ app/Main.hs
@@ -34,14 +34,12 @@ main = do
     path : "delete" : (stripWhitespace -> Just str) : [] ->
       loadFrecencies path >>= writeFrecencies path . delete str
     path : "view" : augs ->
-      loadFrecencies path >>= putStr . unlines . view . augment (mapMaybe stripWhitespace augs)
+      loadFrecencies path >>= putStr . unlines . view . augment (mapMaybe stripWhitespace augs) . expire . decay now
     path : "scores" : augs -> do
       loadFrecencies path >>= printScores . augment (mapMaybe stripWhitespace augs) . expire . decay now
     _ -> die "usage: frecently PATH <bump STR|delete STR|view STR*|scores STR*>"
 
 type Time = Word64
-
-type Score = Double
 
 newtype NEString = NEString {unNEString :: String}
   deriving newtype (Eq, Ord, Show, Serialize)
@@ -49,37 +47,49 @@ newtype NEString = NEString {unNEString :: String}
 getUTC :: IO Time
 getUTC = read <$> readProcess "date" ["+%s"] ""
 
+data Energy = Energy
+  { hourly :: Double,
+    daily :: Double,
+    monthly :: Double
+  }
+  deriving stock (Generic)
+  deriving anyclass (Serialize)
+
+instance Semigroup Energy where Energy h d m <> Energy h' d' m' = Energy (h + h') (d + d') (m + m')
+
 data Frecencies = Frecencies
   { lastUpdate :: Time,
-    frecencies :: Map NEString Score
+    energies :: Map NEString Energy
   }
   deriving stock (Generic)
   deriving anyclass (Serialize)
 
 decay :: Time -> Frecencies -> Frecencies
-decay now (Frecencies prev m) = Frecencies now ((* α) <$> m)
+decay tNow (Frecencies tUpdate frecs) = Frecencies tNow (fmap f frecs)
   where
-    α :: Double
-    α =
-      let halfLife = 30 * 24 * 60 * 60 -- seconds in 30 days
-          deltaT = fromIntegral (now - prev)
-          halfLives = deltaT / halfLife
-       in 0.5 ** halfLives
+    f (Energy h d m) = Energy (alphaHour * h) (alphaDay * d) (alphaMonth * m)
+    deltaSec = fromIntegral $ tNow - tUpdate
+    alphaHour = 0.5 ** (deltaSec / 3600)
+    alphaDay = 0.5 ** (deltaSec / 86400)
+    alphaMonth = 0.5 ** (deltaSec / 2592000)
+
+score :: Energy -> Double
+score (Energy h d m) = h + d + m
 
 delete :: NEString -> Frecencies -> Frecencies
 delete str (Frecencies t fs) = Frecencies t (Map.delete str fs)
 
 expire :: Frecencies -> Frecencies
-expire (Frecencies t fs) = Frecencies t (Map.filter (> 0.1) fs)
+expire (Frecencies t fs) = Frecencies t (Map.filter ((> 0.2) . score) fs)
 
 bump :: NEString -> Frecencies -> Frecencies
-bump str (Frecencies t fs) = Frecencies t (Map.insertWith (+) str 1 fs)
+bump str (Frecencies t fs) = Frecencies t (Map.insertWith (<>) str (Energy 720 30 1) fs)
 
 augment :: [NEString] -> Frecencies -> Frecencies
-augment strs (Frecencies t fs) = Frecencies t (foldr (\str -> Map.insertWith (\_ old -> old) str 0) fs strs)
+augment strs (Frecencies t fs) = Frecencies t (foldr (\str -> Map.insertWith (<>) str (Energy 0 0 0)) fs strs)
 
 view :: Frecencies -> [String]
-view = fmap (unNEString . fst) . sortOn (negate . snd) . Map.toList . frecencies
+view = fmap (unNEString . fst) . sortOn (negate . score . snd) . Map.toList . energies
 
 stripWhitespace :: String -> Maybe NEString
 stripWhitespace str = if null str' then Nothing else Just (NEString str)
@@ -97,6 +107,7 @@ writeFrecencies :: FilePath -> Frecencies -> IO ()
 writeFrecencies path fs = BS.writeFile path (encode fs)
 
 printScores :: Frecencies -> IO ()
-printScores (Frecencies t fs) =
-  forM_ (sortOn (negate . snd) $ Map.toList fs) $ \(str, score) -> do
-    printf "%.5f\t\t%s\n" score (unNEString str)
+printScores (Frecencies _ fs) = do
+  printf "total\t\thourly\t\tdaily\t\tmonthly\n"
+  forM_ (sortOn (negate . score . snd) $ Map.toList fs) $ \(str, Energy h d m) -> do
+    printf "%12.6f\t%12.6f\t%12.6f\5%12.6f\t%s\n" (h + d + m) h d m (unNEString str)
